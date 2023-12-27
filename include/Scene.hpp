d18ebb3ff01e07ce7e93c770906b00cbf79c0f0c
David Oliver Sipos <davidsipos1002@gmail.com>
2023-12-27 20:08:31 +0200

    Generate basic scene and fire

--- include/Scene.hpp
+++ include/Scene.hpp
@@ -7,11 +7,17 @@ using namespace gps;
 class Scene : public Application
 {
    private:
+       
+       ParticleEmitter *emitter;
+       ParticleEmitter *emitter1;
+
         void init() override;
         void update(double delta) override;
         bool doCustomRender() override;
         void render() override;
         void cleanup() override;
+        void generateGround(int x, int y);
+        void generateTreesAndMushrooms(int x, int y);
         
     public:
         Scene(bool fullScreen, int width, int height, const std::string &title);
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -7,10 +7,81 @@ Scene::Scene(bool fullScreen, int width, int height, const std::string &title) :
 void Scene::init()
 {
     skyBox = loader->loadSkyBox("nightsky");
+    std::vector<std::string> filenames;
+    filenames.push_back("fire_particles/1.png");
+    filenames.push_back("fire_particles/2.png");
+    filenames.push_back("fire_particles/3.png");
+    filenames.push_back("fire_particles/4.png");
+
+    renderer->getDirectionalLight(0).intensity = 0.1f;
+    renderer->getDirectionalLight(0).lightColor = glm::vec3(1);
+    renderer->getDirectionalLight(0).lightDirection = glm::vec3(0, 1, 1);
+
+    generateGround(10, 10);
+    generateTreesAndMushrooms(10, 10);
+    Entity *cabin = loader->loadEntity("models/forest_cabin/forest_cabin.obj");
+    cabin->position = glm::vec3(0, -1, 0);
+    cabin->scale = glm::vec3(0.1, 0.1, 0.1);
+    cabin->ambientStrength = 0.2f;
+    cabin->specularStrength = 0.5f;
+    renderer->addEntity(cabin);
+
+    ParticleTexture *texture = loader->loadParticleTexture(filenames, false);
+    emitter = new ParticleEmitter(particleManager, texture, glm::vec3(1, -1, 1), 200, glm::vec3(0.5, 0.5, 0.5), 0, 0.5, 0.5);
+    emitter1 = new ParticleEmitter(particleManager, texture, glm::vec3(1, -1, 1), 200, glm::vec3(0.5, 0.5, 0.5), 0, 0.5, 0.5);
+    renderer->fogColor = 1 / 255.0f * glm::vec3(12, 16, 31);
+    renderer->fogDensity = 0.04f;
+}
+
+void Scene::generateGround(int x, int y)
+{
+    for (int i = -x / 2; i < x / 2; i++)
+    {
+        for (int j = -y / 2; j < y / 2; j++)
+        {
+            Entity *tile = loader->loadEntity("models/ground/ground.obj");
+            tile->position = glm::vec3(i * 2, -1, j * 2);
+            tile->scale = glm::vec3(0.1, 0.1, 0.1);
+            tile->ambientStrength = 0.2f;
+            tile->specularStrength = 0.5f;
+            renderer->addEntity(tile);
+        }
+    }
+}
+
+void Scene::generateTreesAndMushrooms(int x, int y)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    for (int i = -x / 2; i < x / 2; i++)
+    {
+        for (int j = -y / 2; j < y / 2; j++)
+        {
+            if (i != 0 && j != 0)
+            {
+                std::uniform_real_distribution<> pos(-1, 1);
+                Entity *tree = loader->loadEntity("models/tree/treePBR.obj");
+                tree->position = glm::vec3(i * 2 + pos(gen), -1, j * 2 + pos(gen));
+                tree->scale = glm::vec3(0.1, 0.1, 0.1);
+                tree->ambientStrength = 0.2f;
+                tree->specularStrength = 0.5f;
+                renderer->addEntity(tree);
+
+                Entity *mushroom = loader->loadEntity("models/mushrooms/Pilz.obj");
+                mushroom->position = glm::vec3(i * 2, -1, j * 2);
+                mushroom->scale = glm::vec3(0.1, 0.1, 0.1);
+                mushroom->ambientStrength = 0.2f;
+                mushroom->specularStrength = 0.5f;
+                renderer->addEntity(mushroom);
+            }
+        }
+    }
 }
 
 void Scene::update(double delta)
 {
+    emitter->emitParticles(delta);
+    emitter1->emitParticles(delta);
 }
 
 bool Scene::doCustomRender()
